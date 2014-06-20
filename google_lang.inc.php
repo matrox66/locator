de205adcad6190e384bf0095d1a0ffb8059fbff7
Lee Garner <lee@leegarner.com>
2014-06-20 10:36:54 -0700

    Add Google Maps languages

---
+++ google_lang.inc.php
@@ -0,0 +1,37 @@
+<?php
+/**
+*   Array of languages supported by Google Maps API V3.
+*   This is to check if the user's ISO language is available.
+*
+*   @author     Lee Garner <lee@leegarner.com>
+*   @copyright  Copyright (c) 2011 Lee Garner <lee@leegarner.com>
+*   @package    locator
+*   @version    1.0.2
+*   @license    http://opensource.org/licenses/gpl-2.0.php 
+*               GNU Public License v2 or later
+*   @filesource
+*/
+
+$GOOG_lang = array(
+'ar',
+'bg', 'bn', 
+'ca', 'cs',
+'da', 'de',
+'el', 'en', 'en-AU', 'en-GB', 'es', 'eu',
+'fa', 'fi', 'fil', 'fr',
+'gl', 'gu', 'hi', 'hr', 'hu',
+'id', 'it', 'iw',
+'ja',
+'kn', 'ko',
+'lt', 'lv',
+'ml', 'mr',
+'nl', 'no',
+'pl', 'pt', 'pt-BR', 'pt-PT',
+'ro', 'ru',
+'sk', 'sl', 'sr', 'sv',
+'tl', 'ta', 'te', 'th', 'tr',
+'uk',
+'vi',
+'zh-CN', 'zh-TW',
+);
+?>
