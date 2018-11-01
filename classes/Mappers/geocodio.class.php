1ee1bfc96c9276d8c1d7959f76c3b50051c3cb2d
Lee Garner <lee@leegarner.com>
2018-11-01 08:54:31 -0700

    Add Geocodio geocoding service

---
+++ classes/Mappers/geocodio.class.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Class for Geocodio Geocoding provider.
+ * https://www.geocodio.io
+ * This provides geocoding only, no map generation.
+ *
+ * @author      Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2018 Lee Garner <lee@leegarner.com>
+ * @package     locator
+ * @version     1.2.0
+ * @since       1.2.0
+ * @license     http://opensource.org/licenses/gpl-2.0.php 
+ *              GNU Public License v2 or later
+ * @filesource
+ */
+namespace Locator\Mappers;
+
+/**
+*   Class to handle the general location markers
+*   @package    locator
+*/
+class geocodio extends \Locator\Mapper
+{
+    private $api_key = NULL;
+    protected $is_geocoder = true;
+    protected $display_name = 'Geocodio';
+    protected $name = 'geocodio';
+    const GEOCODE_URL = 'https://api.geocod.io/v1.3/geocode?api_key=%s&q=%s';
+
+    /**
+    *   Constructor
+    *
+    *   @param  string  $id     Optional ID of a location to load
+    */
+    public function __construct($id = '')
+    {
+        global $_CONF_GEO;
+        if (isset($_CONF_GEO['geocodio_api_key'])) {
+            $this->api_key = $_CONF_GEO['geocodio_api_key'];
+        }
+    }
+
+
+    /**
+     * Get the coordinates from an address string.
+     *
+     * @param   string  $address    Address string
+     * @param   float   &$lat       Latitude return var
+     * @param   float   &$lng       Longitude return var
+     * @return  integer             0 for success, nonzero for failure
+     */
+    public function geoCode($address, &$lat, &$lng)
+    {
+        $cache_key = $this->getName() . '_geocode_' . md5($address);
+        $data = \Locator\Cache::get($cache_key);
+        if ($data === NULL) {
+            $url = sprintf(self::GEOCODE_URL, $this->api_key, urlencode($address));
+            $json = self::getUrl($url);
+            $data = json_decode($json, true);
+            if (!isset($data['results'][0]['location']) || empty($data['results'][0]['location'])) {
+                // Didn't get even one result
+                return -1;
+            }
+            \Locator\Cache::set($cache_key, $data);
+        }
+
+        // Get the most accurate result
+        $acc_code = -1;     // Initialize accuracy code
+        foreach ($data['results'] as $idx=>$loc_data) {
+            $loc_acc_code = (float)$loc_data['accuracy'];
+            if ($loc_acc_code > $acc_code) {
+                $acc_code = $loc_acc_code;
+                $loc_idx = $idx;
+            }
+        }
+        $loc = $data['results'][$idx]['location'];
+
+        if (!isset($loc['lat']) || !isset($loc['lng'])) {
+            $lat = 0;
+            $lng = 0;
+            return -1;
+        } else {
+            $lat = $loc['lat'];
+            $lng = $loc['lng'];
+            return 0;
+        }
+    }
+
+}
+
+?>
